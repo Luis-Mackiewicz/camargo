aa6502bef91e0490d3a365122bdffbff0ee19498
luis mackiewicz <luismiguelpix@gmail.com>
2026-03-03 16:26:32 -0300

    feat(header): add hover transition

--- components/header.tsx
+++ components/header.tsx
@@ -4,20 +4,20 @@ export default function Header() {
   return (
     <header className="h-12 w-full flex items-center justify-center fixed top-0 border-b border-white/10 z-50 md:h-14">
       <nav className="md:w-4/5">
-        <ul className="flex gap-4 text-gray-300 text-sm md:text-base md:justify-end md:gap-8">
-          <li className="">
+        <ul className="flex gap-4 text-white text-sm md:text-base md:justify-end md:gap-8">
+          <li className="hover:opacity-40 transition-all duration-700">
             <Link href="/">Início</Link>
           </li>
-          <li className="">
+          <li className="hover:opacity-40 transition-all duration-700">
             <Link href="/sobre">sobre</Link>
           </li>
-          <li className="">
+          <li className="hover:opacity-40 transition-all duration-700">
             <Link href="/servicos">serviços</Link>
           </li>
-          <li className="">
+          <li className="hover:opacity-40 transition-all duration-700">
             <Link href="/agendar">agendar</Link>
           </li>
-          <li className="">
+          <li className="hover:opacity-40 transition-all duration-700">
             <Link href="/videos">vídeos</Link>
           </li>
         </ul>
