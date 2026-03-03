963103d621b3f66deff7df07a2ecf96fd3427947
luis mackiewicz <luismiguelpix@gmail.com>
2026-03-03 12:27:10 -0300

    feat: create header

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -2,24 +2,26 @@ import Link from "next/link";
 
 export default function Header() {
   return (
-    <nav>
-      <ul>
-        <li>
-          <Link href="">Início</Link>
-        </li>
-        <li>
-          <Link href="">sobre</Link>
-        </li>
-        <li>
-          <Link href="">serviços</Link>
-        </li>
-        <li>
-          <Link href="">agendar</Link>
-        </li>
-        <li>
-          <Link href="">vídeos</Link>
-        </li>
-      </ul>
-    </nav>
+    <header className="h-12 w-full fixed top-0 border-b border-white/10">
+      <nav className="">
+        <ul>
+          <li>
+            <Link href="/">Início</Link>
+          </li>
+          <li>
+            <Link href="/sobre">sobre</Link>
+          </li>
+          <li>
+            <Link href="/servicos">serviços</Link>
+          </li>
+          <li>
+            <Link href="/agendar">agendar</Link>
+          </li>
+          <li>
+            <Link href="/videos">vídeos</Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
   );
 }
