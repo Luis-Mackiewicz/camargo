ecc7f6b360b6104f204c9d99f1f6ecb2621e6de0
luis mackiewicz <luismiguelpix@gmail.com>
2026-03-03 15:50:14 -0300

    feat(pages): add background images

--- app/videos/page.tsx
+++ app/videos/page.tsx
@@ -1,3 +1,10 @@
 export default function videos() {
-  return <div className="h-dvh w-full"></div>;
+  return (
+    <main className="relative h-dvh w-full flex items-center justify-center">
+      <div className="absolute inset-0 -z-10">
+        <div className="absolute inset-0 bg-linear-to-t from-blue-700/80 via-blue-800/60 to-gray-900 z-1/90" />
+        <div className="absolute inset-0 bg-[url('/bg-images/background-videos.svg')] bg-cover bg-center opacity-40" />
+      </div>
+    </main>
+  );
 }
