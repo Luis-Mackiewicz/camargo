c262e035e62e551583081456ebbb610ee679646b
luis mackiewicz <luismiguelpix@gmail.com>
2026-03-03 16:47:33 -0300

    feat: create list of areas of specialization

---
+++ constants/areas.ts
@@ -0,0 +1,38 @@
+export const areasOfSpecialization = [
+  {
+    icone: "/service-icons/book.svg",
+    nome: "Direito Previdenciário",
+    description:
+      "Assessoria em benefícios do INSS, aposentadorias, auxílio-doença, BPC/LOAS e revisões de cálculos, garantindo proteção e segurança ao trabalhador e sua família.",
+  },
+  {
+    icone: "/public/service-icons/familly.svg",
+    nome: "Direito da Família",
+    description:
+      "Atuação em divórcios, pensão alimentícia, guarda de filhos, inventários, união estável e outras questões familiares, sempre com sensibilidade e foco na solução pacífica.",
+  },
+  {
+    icone: "/service-icons/book.svg",
+    nome: "Direito do consumidor",
+    description:
+      "Defesa dos direitos do consumidor em casos de cobranças indevidas, vícios em produtos ou serviços, contratos abusivos e ações contra empresas.",
+  },
+  {
+    icone: "/service-icons/book.svg",
+    nome: "Direito Público",
+    description:
+      "Atuação em causas envolvendo o poder público, como concursos, licitações, servidor público, improbidade administrativa e ações contra o Estado.",
+  },
+  {
+    icone: "/service-icons/book.svg",
+    nome: "Registro de Marcas",
+    description:
+      "Auxílio completo no processo de registro de marcas junto ao INPI, incluindo buscas prévias, acompanhamento do processo e defesa contra oposições.",
+  },
+  {
+    icone: "/service-icons/book.svg",
+    nome: "Mediação",
+    description:
+      "Resolução de conflitos de forma rápida, eficaz e amigável, sem a necessidade de processo judicial, promovendo acordos vantajosos para ambas as partes.",
+  },
+];
