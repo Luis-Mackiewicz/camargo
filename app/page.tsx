45703392da713d357c2ae4dc5f8cad316125c116
luis mackiewicz <luismiguelpix@gmail.com>
2026-03-03 14:01:39 -0300

    feat(home): import card

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,15 @@
+import {
+  Card,
+  CardAction,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/card";
+
 export default function Home() {
-  return <main className="h-dvh w-full"></main>;
+  return (
+    <main className="h-dvh w-full flex items-center justify-center"></main>
+  );
 }
