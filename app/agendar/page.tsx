b81552b2c1b439b8fe1ee5165dc501433812574e
luis mackiewicz <luismiguelpix@gmail.com>
2026-03-05 09:59:43 -0300

    feat(agendar): add map and style form

--- app/agendar/page.tsx
+++ app/agendar/page.tsx
@@ -14,10 +14,22 @@ import {
 import { Textarea } from "@/components/textarea";
 import { Button } from "@/components/button";
 
+function Map() {
+  return (
+    <iframe
+      src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d3640.8260015049045!2d-52.7821633!3d-24.142748!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94f280d61498ce6b%3A0x82a00122dd0aa7c5!2sCamargo%20Advocacia%20e%20Assessoria%20Jur%C3%ADdica!5e0!3m2!1spt-BR!2sbr!4v1772710914354!5m2!1spt-BR!2sbr"
+      loading="lazy"
+      allowFullScreen
+      referrerPolicy="no-referrer-when-downgrade"
+      className="w-4/5 h-3/4 rounded-xl md:w-full md:h-auto lg:h-3/4"
+    ></iframe>
+  );
+}
+
 function SelectOption() {
   return (
     <Select>
-      <SelectTrigger className="w-full max-w-48">
+      <SelectTrigger className="w-full">
         <SelectValue placeholder="áreas do direito" />
       </SelectTrigger>
       <SelectContent>
@@ -45,21 +57,48 @@ export default function Agendar() {
         <div className="absolute inset-0 bg-[url('/bg-images/background-schedulling.svg')] bg-cover bg-center opacity-40" />
       </div>
 
-      <div className="h-3/4 w-4/5 bg-blue-950 border-0 shadow-2xl rounded-xl">
-        <div className="bg-red-500"></div>
-        <form action="" className="w-4/5 text-black bg-white">
-          <Input type="text" placeholder="nome" />
-          <Input type="email" placeholder="email" />
-          <Input type="number" placeholder="número" />
-          <SelectOption />
-          <Textarea className="" />
-          <Button
-            variant="default"
-            className="curosr-pointer rounded-2xl transition-all shadow-2xl bg-blue-800 duration-300 hover:scale-110 hover:cursor-pointer hover:bg-linear-to-r from-green-500 to-green-700"
-          >
-            enviar
-            <SendHorizonal className="w-4 h-4" />
-          </Button>
+      <div className="h-3/4 w-4/5 grid grid-cols-1 grid-rows-3 bg-blue-950 border-0 shadow-2xl rounded-2xl md:grid-cols-3 md:grid-rows-1">
+        <aside className="p-2 flex flex-col items-center justify-center gap-2 md:p-8 lg:p-16">
+          <Image
+            src={CamargoLogo}
+            alt="logo camargo advocacia"
+            loading="eager"
+            className="hidden lg:flex"
+          />
+          <p className="text-white text-base font-bold">Localização:</p>
+          <Map />
+          <div></div>
+        </aside>
+        <form
+          action=""
+          className="flex items-center justify-center  bg-white/95 row-span-2 md:col-span-2 rounded-b-xl md:rounded-xl md:rounded-l-none"
+        >
+          <fieldset className="w-4/5  flex flex-col gap-2.5">
+            <Input
+              type="text"
+              placeholder="nome"
+              className="selection:bg-blue-900"
+            />
+            <Input
+              type="email"
+              placeholder="email"
+              className="selection:bg-blue-900"
+            />
+            <Input
+              type="number"
+              placeholder="número"
+              className="selection:bg-blue-900"
+            />
+            <SelectOption />
+            <Textarea />
+            <Button
+              variant="default"
+              className="curosr-pointer rounded-2xl transition-all shadow-2xl bg-blue-900 duration-300 hover:scale-105 hover:cursor-pointer hover:bg-linear-to-r from-green-500 to-green-700"
+            >
+              enviar
+              <SendHorizonal className="w-4 h-4" />
+            </Button>
+          </fieldset>
         </form>
       </div>
     </main>
