1c7ffc508702866594442eaccdb0e732387ff06a
luis mackiewicz <luismiguelpix@gmail.com>
2026-03-05 11:45:33 -0300

    feat(agendar): add zod validation and react hook form

--- app/agendar/page.tsx
+++ app/agendar/page.tsx
@@ -1,7 +1,7 @@
+"use client";
+
+import { Button } from "@/components/button";
 import { Input } from "@/components/input";
-import Image from "next/image";
-import CamargoLogo from "@/public/logo.svg";
-import { SendHorizonal } from "lucide-react";
 import {
   Select,
   SelectContent,
@@ -12,9 +12,27 @@ import {
   SelectValue,
 } from "@/components/select";
 import { Textarea } from "@/components/textarea";
-import { Button } from "@/components/button";
+import CamargoLogo from "@/public/logo.svg";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { SendHorizonal } from "lucide-react";
+import Image from "next/image";
+import { useForm } from "react-hook-form";
+import * as z from "zod";
 
-function Map() {
+const formSchema = z.object({
+  name: z
+    .string()
+    .min(3, "O nome precisa ter pelo menos 3 caracteres")
+    .max(255, "O nome não pode ter mais de 255 caracteres"),
+
+  email: z.string().email("Email inválido"),
+  phone: z.string().min(10, "Telefone inválido"),
+  message: z.string().min(10, "Mensagem precida de no minimo 10 caracteres!"),
+});
+
+type FormData = z.infer<typeof formSchema>;
+
+const Map = () => {
   return (
     <iframe
       src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d3640.8260015049045!2d-52.7821633!3d-24.142748!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94f280d61498ce6b%3A0x82a00122dd0aa7c5!2sCamargo%20Advocacia%20e%20Assessoria%20Jur%C3%ADdica!5e0!3m2!1spt-BR!2sbr!4v1772710914354!5m2!1spt-BR!2sbr"
@@ -24,9 +42,9 @@ function Map() {
       className="w-4/5 h-3/4 rounded-xl md:w-full md:h-auto lg:h-3/4"
     ></iframe>
   );
-}
+};
 
-function SelectOption() {
+const SelectOption = () => {
   return (
     <Select>
       <SelectTrigger className="w-full">
@@ -47,9 +65,21 @@ function SelectOption() {
       </SelectContent>
     </Select>
   );
-}
+};
 
 export default function Agendar() {
+  const form = useForm<FormData>({
+    resolver: zodResolver(formSchema),
+    mode: "onTouched",
+    defaultValues: {
+      name: "",
+      email: "",
+      phone: "",
+      message: "",
+    },
+  });
+
+  const onSubmit = (data: FormData) => console.log(data);
   return (
     <main className="relative h-dvh w-full flex items-center justify-center">
       <div className="absolute inset-0 -z-10">
@@ -70,30 +100,71 @@ export default function Agendar() {
           <div></div>
         </aside>
         <form
+          onSubmit={form.handleSubmit(onSubmit)}
           action=""
           className="flex items-center justify-center  bg-white/95 row-span-2 md:col-span-2 rounded-b-xl md:rounded-xl md:rounded-l-none"
         >
-          <fieldset className="w-4/5  flex flex-col gap-2.5">
-            <Input
-              type="text"
-              placeholder="nome"
-              className="selection:bg-blue-900"
-            />
-            <Input
-              type="email"
-              placeholder="email"
-              className="selection:bg-blue-900"
-            />
-            <Input
-              type="number"
-              placeholder="número"
-              className="selection:bg-blue-900"
-            />
-            <SelectOption />
-            <Textarea />
+          <fieldset className="w-4/5  flex flex-col gap-2.5 md:gap-5">
+            <div className="flex flex-col">
+              <Input
+                type="text"
+                placeholder="nome"
+                className="selection:bg-blue-900"
+                {...form.register("name")}
+              />
+
+              {form.formState.errors.name && (
+                <p className="text-red-500 text-sm">
+                  {form.formState.errors.name.message}
+                </p>
+              )}
+            </div>
+
+            <div className="flex flex-col">
+              <Input
+                type="email"
+                placeholder="email"
+                className="selection:bg-blue-900"
+                {...form.register("email")}
+              />
+
+              {form.formState.errors.email && (
+                <p className="text-red-500 text-sm">
+                  {form.formState.errors.email.message}
+                </p>
+              )}
+            </div>
+
+            <div className="flex flex-col">
+              <Input
+                type="tel"
+                placeholder="número"
+                className="selection:bg-blue-900"
+                {...form.register("phone")}
+              />
+
+              {form.formState.errors.phone && (
+                <p className="text-red-500 text-sm">
+                  {form.formState.errors.phone.message}
+                </p>
+              )}
+            </div>
+            <div className="flex flex-col">
+              <SelectOption />
+            </div>
+            <div className="flex flex-col">
+              <Textarea {...form.register("message")} />
+            </div>
+
+            {form.formState.errors.message && (
+              <p className="text-red-500 text-sm">
+                {form.formState.errors.message.message}
+              </p>
+            )}
             <Button
               variant="default"
-              className="curosr-pointer rounded-2xl transition-all shadow-2xl bg-blue-900 duration-300 hover:scale-105 hover:cursor-pointer hover:bg-linear-to-r from-green-500 to-green-700"
+              className="cursor-pointer rounded-2xl transition-all shadow-2xl bg-blue-900 duration-300 hover:scale-105 hover:cursor-pointer hover:bg-linear-to-r from-green-500 to-green-700"
+              disabled={!form.formState.isValid}
             >
               enviar
               <SendHorizonal className="w-4 h-4" />
