bb31396c7af0ee3451aa2da6462047cefe7ca338
luis mackiewicz <luismiguelpix@gmail.com>
2026-03-27 13:36:30 -0300

    feat(servicos): add new styles

--- app/servicos/page.tsx
+++ app/servicos/page.tsx
@@ -13,27 +13,25 @@ export default function Servicos() {
 
   return (
     <>
-      <ServicosBackground />
-      <main className="min-h-dvh w-full flex items-center justify-center">
-        <div className="max-w-6xl w-full text-white flex flex-col gap-16">
-          <section className="max-w-2xl space-y-4">
-            <h1 className="text-4xl md:text-6xl font-bold leading-tight">
-              Áreas de atuação
-            </h1>
+      <main className="h-dvh w-full flex items-center justify-center overflow-hidden">
+        <ServicosBackground />
+        <div className="w-4/5 px-4 text-white flex flex-col gap-16">
+          <section className="w-full max-w-3xl">
+            <h1 className="text-xl md:text-4xl font-bold">Serviços</h1>
 
-            <p className="text-gray-300 text-base leading-relaxed">
-              Excelência jurídica e compromisso com o seu direito. Atuamos com
-              estratégia, precisão e foco em resultados para proteger o que é
-              importante para você.
+            <p className="text-gray-300 text-base leading-relaxed mt-4">
+              Atuamos na defesa dos seus direitos com atendimento ágil, análise
+              estratégica e atuação precisa em cada etapa do processo. Nosso
+              compromisso é buscar a melhor solução jurídica para o seu caso.
             </p>
           </section>
 
-          <section className="grid grid-cols-2 sm:grid-cols-3 gap-24 md:p-8">
+          <section className="border border-white/10 rounded-2xl w-full grid grid-cols-2 p-1 sm:grid-cols-3 gap-12 lg:p-30">
             {areasOfSpecialization.map((area) => (
               <div
                 key={area.id}
                 onClick={() => setSelected(area)}
-                className="group flex flex-col items-center text-center gap-4 cursor-pointer transition-all"
+                className="group flex flex-col items-center text-center gap-4 cursor-pointer"
               >
                 <div
                   className="
@@ -42,9 +40,9 @@ export default function Servicos() {
                   rounded-2xl
                   bg-linear-to-br from-blue-600 to-blue-800
                   shadow-lg
+                  transition-all
                   group-hover:scale-110
                   group-hover:shadow-blue-900/50
-                  transition-all
                 "
                 >
                   <Image
