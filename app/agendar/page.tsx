944bc97da4957cec23eb52209be72d518a2e3bea
luis mackiewicz <luismiguelpix@gmail.com>
2026-03-31 20:19:21 -0300

    fix(map): Enter the correct address.

--- app/agendar/page.tsx
+++ app/agendar/page.tsx
@@ -41,7 +41,7 @@ type SelectOptionProps = {
 const Map = () => {
   return (
     <iframe
-      src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d14515.688846387!2d-52.3551!3d-24.1256!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1spt-BR!2sbr!4v1710000000000"
+      src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3640.8257826739728!2d-52.78461122382201!3d-24.14275568321062!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94f280d61498ce6b%3A0x82a00122dd0aa7c5!2sCamargo%20Advocacia%20e%20Assessoria%20Jur%C3%ADdica!5e0!3m2!1spt-BR!2sus!4v1774999002181!5m2!1spt-BR!2sus"
       loading="lazy"
       allowFullScreen
       referrerPolicy="no-referrer-when-downgrade"
